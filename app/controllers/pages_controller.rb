de25f77492802473d1ce363b952f13de634de3fe
Huthaifa-Taya <thuthaifa@everconnect.com>
2023-01-26 18:48:59 +0300

    desgined the header of the site

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home; end
+
+  def add_product; end
+
+  def products; end
+
+  def categories; end
+
+  def about; end
+
+  def contact_us; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'pages/add_product'
+  get '/products', to: 'pages#products'
+  get '/categories', to: 'pages#categories'
+  get '/about', to: 'pages#about'
+  get '/contact_us', to: 'pages#contact_us'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
